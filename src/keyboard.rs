c63345e8da20c73c9e78eaa581ffd9d96dbe2325
semi <semilinares@pm.me>
2022-01-31 10:20:37 -0800

    Add dir_to function

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -11,6 +11,18 @@ impl Finger {
     pub fn new(hand: Hand, kind: FingerKind) -> Finger {
 	Finger { hand, kind }
     }
+    /// Returns the direction between self and the argument f.
+    /// ```rust
+    /// use keynergy::keyboard::{Finger, Hand, FingerKind, Direction};
+    /// let ri = Finger::new(Hand::Right, FingerKind::Index);
+    /// let rm = Finger::new(Hand::Right, FingerKind::Middle);
+    /// assert_eq!(Direction::Outward, ri.dir_to(rm));
+    /// assert_eq!(Direction::Inward, rm.dir_to(ri));
+    /// assert_eq!(Direction::None, ri.dir_to(ri));
+    /// ```
+    pub fn dir_to(self, f: Finger) -> Direction {
+	direction(self, f)
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
